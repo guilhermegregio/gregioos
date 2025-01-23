2718524e7a6c7a38674b7392233a543e53a5411a
installer <installer@gmail.com>
2025-01-22 21:40:03 -0300

    chore: initial structure

---
+++ hosts/home.nix
@@ -0,0 +1,77 @@
+{
+  pkgs,
+  username,
+  host,
+  ...
+}:
+let
+  inherit (import ./variables.nix) gitUsername gitEmail;
+in
+{
+  # Home Manager Settings
+  home.username = "${username}";
+  home.homeDirectory = "/home/${username}";
+  home.stateVersion = "24.11";
+
+  # Import Program Configurations
+  imports = [
+  ];
+
+  # Install & Configure Git
+  programs.git = {
+    enable = true;
+    userName = "${gitUsername}";
+    userEmail = "${gitEmail}";
+  };
+
+  programs = {
+    fastfetch.enable = true;
+    gh.enable = true;
+    btop = {
+      enable = true;
+      settings = {
+        vim_keys = true;
+      };
+    };
+    kitty = {
+      enable = true;
+      package = pkgs.kitty;
+      settings = {
+        scrollback_lines = 2000;
+        wheel_scroll_min_lines = 1;
+        window_padding_width = 4;
+        confirm_os_window_close = 0;
+      };
+      extraConfig = ''
+        tab_bar_style fade
+        tab_fade 1
+        active_tab_font_style   bold
+        inactive_tab_font_style bold
+      '';
+    };
+     starship = {
+        enable = true;
+        package = pkgs.starship;
+     };
+    bash = {
+      enable = true;
+      enableCompletion = true;
+      profileExtra = ''
+      '';
+      initExtra = ''
+        fastfetch
+      '';
+      shellAliases = {
+        fr = "nh os switch --hostname ${host} /home/${username}/gregioos";
+        fu = "nh os switch --hostname ${host} --update /home/${username}/gregioos";
+        ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+        cat = "bat";
+        ls = "eza --icons";
+        ll = "eza -lh --icons --grid --group-directories-first";
+        la = "eza -lah --icons --grid --group-directories-first";
+        ".." = "cd ..";
+      };
+    };
+    home-manager.enable = true;
+  };
+}
---
+++ hosts/variables.nix
@@ -0,0 +1,16 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Guilherme Gregio";
+  gitEmail = "guilherme@gregio.net";
+
+  # Hyprland Settings
+  extraMonitorSettings = "";
+
+  # Waybar Settings
+  clock24h = false;
+
+  # Program Options
+  browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+}
