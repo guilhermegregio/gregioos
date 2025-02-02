b13b45b3cb74cb713850a80d7e19833ea5d507bc
Guilherme Gregio <guilherme@gregio.net>
2025-02-02 10:45:12 -0300

    fix: ident

--- home/bash.nix
+++ home/bash.nix
@@ -9,27 +9,26 @@ let
 in
 {
   programs.bash = {
-      enable = true;
-      enableCompletion = true;
+    enable = true;
+    enableCompletion = true;
 
-      profileExtra = ''
-      '';
+    profileExtra = ''
+    '';
 
-      initExtra = ''
-        fastfetch
-      '';
+    initExtra = ''
+      fastfetch
+    '';
 
-      shellAliases = {
-        g = "git";
-        fr = "nh os switch --hostname ${host} /home/${username}/gregioos";
-        fu = "nh os switch --hostname ${host} --update /home/${username}/gregioos";
-        ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
-        cat = "bat";
-        ls = "eza --icons";
-        ll = "eza -lh --icons --grid --group-directories-first";
-        la = "eza -lah --icons --grid --group-directories-first";
-        ".." = "cd ..";
-      };
+    shellAliases = {
+      g = "git";
+      fr = "nh os switch --hostname ${host} /home/${username}/gregioos";
+      fu = "nh os switch --hostname ${host} --update /home/${username}/gregioos";
+      ncg = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
+      cat = "bat";
+      ls = "eza --icons";
+      ll = "eza -lh --icons --grid --group-directories-first";
+      la = "eza -lah --icons --grid --group-directories-first";
+      ".." = "cd ..";
     };
   };
 }
