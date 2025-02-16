185fcd83596046ad0770b7b33a73c8666e99179f
Guilherme Gregio <guilherme@gregio.net>
2025-02-16 16:22:16 -0300

    chore: update

--- hosts/gregio-note/config.nix
+++ hosts/gregio-note/config.nix
@@ -85,7 +85,7 @@ in
 
   # Enable automatic login for the user.
   services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "gregio";
+  services.displayManager.autoLogin.user = "${username}";
 
   # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
   systemd.services."getty@tty1".enable = false;
