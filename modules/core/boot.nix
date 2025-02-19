5200b2837aa28ff5a4ebebbe031063d03cddf135
Guilherme Gregio <guilherme@gregio.net>
2025-02-19 07:23:05 -0300

    feat: boot appimage

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -2,23 +2,25 @@
 {
   boot = {
     kernelPackages = pkgs.linuxPackages_zen;
-    # kernelModules = [ "v4l2loopback" ];
-    kernelModules = [ "kvm-intel" ];
-    # extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
-    extraModulePackages = [ ];
+    kernelModules = [ "v4l2loopback" ];
+    # kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    # extraModulePackages = [ ];
     # kernel.sysctl = { "vm.max_map_count" = 2147483642; };
 
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
+
     # Appimage Support
-    # binfmt.registrations.appimage = {
-    #   wrapInterpreterInShell = false;
-    #   interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-    #   recognitionType = "magic";
-    #   offset = 0;
-    #   mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-    #   magicOrExtension = ''\x7fELF....AI\x02'';
-    # };
-    # plymouth.enable = true;
+    binfmt.registrations.appimage = {
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+      magicOrExtension = ''\x7fELF....AI\x02'';
+    };
+
+    plymouth.enable = true;
   };
 }
