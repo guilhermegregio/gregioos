8e0be7221b90fd0ad450fb455b1e543fce1fbd01
Guilherme Gregio <guilherme@gregio.net>
2025-02-19 10:18:40 -0300

    chore: update

--- hosts/gregio-note/variables.nix
+++ hosts/gregio-note/variables.nix
@@ -12,7 +12,9 @@
   # Program Options
   browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
   terminal = "kitty"; # Set Default System Terminal
+
   keyboardLayout = "us";
+  consoleKeyMap = "us";
 
   stateVersion = "24.11";
 }
