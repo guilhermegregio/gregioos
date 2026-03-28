7c9a1abc5d90e94b744c57acdb6cb7bfecb04117
Guilherme Gregio <guilherme@gregio.net>
2026-03-28 11:19:25 -0300

    chore: lint

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,11 +1,6 @@
-{
-  host,
-  ...
-}:
-let
-  inherit (import ../../hosts/${host}/variables.nix) gitUsername gitEmail;
-in
-{
+{ host, ... }:
+let inherit (import ../../hosts/${host}/variables.nix) gitUsername gitEmail;
+in {
   programs.delta = {
     enable = true;
     enableGitIntegration = true;
@@ -33,36 +28,39 @@ in
         p = "push";
         df = "diff --color --color-words --abbrev";
         co = "checkout";
-        lg = "log --graph --pretty=format:'%Cred%h%Creset %Cgreen(%cr) %C(yellow)%d%Creset - %s %C(bold blue)<%an>%Creset'";
-        d = "!\"git diff-index --quiet HEAD -- || clear; git --no-pager diff --patch-with-stat\"";
+        lg =
+          "log --graph --pretty=format:'%Cred%h%Creset %Cgreen(%cr) %C(yellow)%d%Creset - %s %C(bold blue)<%an>%Creset'";
+        d =
+          "!git diff-index --quiet HEAD -- || clear; git --no-pager diff --patch-with-stat";
         ignore = "!gi() { curl -L -s https://www.gitignore.io/api/$@ ;}; gi";
-        pb = "!\"git fetch --all -p && git branch -vv | rg -v '^\\*' | rg ': gone]' | awk '{ print $1 }' | xargs -r -n 1 git branch -D\"";
+        pb =
+          "!git fetch --all -p && git branch -vv | rg -v '^\\*' | rg ': gone]' | awk '{ print $1 }' | xargs -r -n 1 git branch -D";
       };
     };
 
     ignores = [
-          # ide
-          ".idea"
-          ".vs"
-          ".vsc"
-          ".vscode"
-          # npm
-          "node_modules"
-          "npm-debug.log"
-          # python
-          "__pycache__"
-          "*.pyc"
+      # ide
+      ".idea"
+      ".vs"
+      ".vsc"
+      ".vscode"
+      # npm
+      "node_modules"
+      "npm-debug.log"
+      # python
+      "__pycache__"
+      "*.pyc"
 
-          ".ipynb_checkpoints" # jupyter
-          "__sapper__" # svelte
-          ".DS_Store" # mac
-          "kls_database.db" # kotlin lsp
-          "result"
-          "tags"
+      ".ipynb_checkpoints" # jupyter
+      "__sapper__" # svelte
+      ".DS_Store" # mac
+      "kls_database.db" # kotlin lsp
+      "result"
+      "tags"
 
-          # nix envs
-          ".envrc"
-          ".direnv"
-        ];
+      # nix envs
+      ".envrc"
+      ".direnv"
+    ];
   };
 }
